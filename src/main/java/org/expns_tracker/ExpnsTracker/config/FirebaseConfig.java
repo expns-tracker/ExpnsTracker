13b358419a117c74be024e68e121da5fd1964156
Vlad Anghelache <anghelache.vlad.1@gmail.com>
2025-11-10 00:00:49 +0200

    Add credentialsProvider bean for authenticating firestore

--- src/main/java/org/expns_tracker/ExpnsTracker/config/FirebaseConfig.java
+++ src/main/java/org/expns_tracker/ExpnsTracker/config/FirebaseConfig.java
@@ -1,10 +1,15 @@
 package org.expns_tracker.ExpnsTracker.config;
 
+import com.google.api.gax.core.CredentialsProvider;
+import com.google.api.gax.core.FixedCredentialsProvider;
 import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.firestore.Firestore;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import com.google.firebase.cloud.FirestoreClient;
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.annotation.PostConstruct;
@@ -29,5 +34,12 @@ public class FirebaseConfig {
             FirebaseApp.initializeApp(options);
             log.info("Firebase initialized successfully");
         }
+    }
+
+    @Bean
+    public CredentialsProvider credentialsProvider() throws IOException {
+        GoogleCredentials credentials = GoogleCredentials
+                .fromStream(new FileInputStream(firebaseProperties.getConfigPath()));
+        return FixedCredentialsProvider.create(credentials);
     }
 }
