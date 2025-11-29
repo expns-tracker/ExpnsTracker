05e68ba5bf2eadad1ce869ead2157a8f01a731b2
Vlad Anghelache <anghelache.vlad.1@gmail.com>
2025-11-30 00:28:46 +0200

    Add redirect logic for login and register when logged in

--- src/main/java/org/expns_tracker/ExpnsTracker/controller/HomeController.java
+++ src/main/java/org/expns_tracker/ExpnsTracker/controller/HomeController.java
@@ -1,5 +1,9 @@
 package org.expns_tracker.ExpnsTracker.controller;
 
+import jakarta.servlet.http.HttpSession;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -13,11 +17,30 @@ public class HomeController {
 
     @GetMapping("/register")
     public String register(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (isAuthenticated()) {
+            return "redirect:/home";
+        }
         return "auth/register";
     }
 
     @GetMapping("/login")
     public String login(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (isAuthenticated()) {
+            return "redirect:/";
+        }
         return "auth/login";
+    }
+
+    private boolean isAuthenticated() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || AnonymousAuthenticationToken.class.
+                isAssignableFrom(authentication.getClass())) {
+            return false;
+        }
+        return authentication.isAuthenticated();
     }
 }
