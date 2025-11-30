d524cebf293d6a356108c4f5c9cb7ffb35b6dbe4
vladanghelache <61271049+vladanghelache@users.noreply.github.com>
2025-11-30 15:29:07 +0200

    Add redirect logic for login and register when logged in (#74)
    * Update security filter
    
    * Add redirect logic for login and register when logged in

--- src/main/java/org/expns_tracker/ExpnsTracker/security/SecurityConfig.java
+++ src/main/java/org/expns_tracker/ExpnsTracker/security/SecurityConfig.java
@@ -3,10 +3,12 @@ package org.expns_tracker.ExpnsTracker.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authorization.AuthorizationDecision;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.expression.WebExpressionAuthorizationManager;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
@@ -18,14 +20,21 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/auth/session-login").anonymous()
                         .requestMatchers(
                                 "/login",
                                 "/register",
-                                "/api/auth/session-login",
+                                "/error",
+                                "/error/**",
                                 "/css/**",
                                 "/js/**",
                                 "/favicon.ico").permitAll()
                         .anyRequest().authenticated()
+                )
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .loginProcessingUrl("/login")
+                        .defaultSuccessUrl("/")
                 )
 //                .addFilterBefore(firebaseTokenFilter, UsernamePasswordAuthenticationFilter.class)
         ;
