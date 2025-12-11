f612c8651cfb910bdbb57c978344607f9a367447
Ludu Dragoș <93485643+Dargoswwwe@users.noreply.github.com>
2025-12-11 19:54:49 +0200

    Story_9 created transaction entity and specific classes (#77)

--- src/main/java/org/expns_tracker/ExpnsTracker/service/UserService.java
+++ src/main/java/org/expns_tracker/ExpnsTracker/service/UserService.java
@@ -3,7 +3,7 @@ package org.expns_tracker.ExpnsTracker.service;
 import lombok.RequiredArgsConstructor;
 import org.expns_tracker.ExpnsTracker.entity.User;
 import org.expns_tracker.ExpnsTracker.repository.UserRepository;
-import org.jetbrains.annotations.NotNull;
+
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.ExecutionException;
@@ -45,7 +45,7 @@ public class UserService {
         }
     }
 
-    @NotNull
+
     private User getUser(String userId) {
         User user;
         try {
