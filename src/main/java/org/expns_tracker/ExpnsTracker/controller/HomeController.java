b532fb95cf4c0dc15f51d6a73e5988a33b3941d4
Andrei-Orzata <andrei.orzata@my.fmi.unibuc.ro>
2025-12-17 19:25:09 +0200

    add-minimal-dashboard

--- src/main/java/org/expns_tracker/ExpnsTracker/controller/HomeController.java
+++ src/main/java/org/expns_tracker/ExpnsTracker/controller/HomeController.java
@@ -11,6 +11,51 @@ public class HomeController {
         return "index";
     }
 
+    @GetMapping("/transactions")
+    public String transactions(){
+        return "transactions";
+    }
+
+    @GetMapping("/new")
+    public String New(){
+        return "new";
+    }
+
+    @GetMapping("/admin")
+    public String admin(){
+        return "admin";
+    }
+
+    @GetMapping("/settings")
+    public String settings(){
+        return "settings";
+    }
+
+    @GetMapping("/profile")
+    public String profile(){
+        return "profile";
+    }
+
+    @GetMapping("/import")
+    public String imports(){
+        return "import";
+    }
+
+    @GetMapping("/dashboard")
+    public String dashboard(){
+        return "dashboard";
+    }
+
+    @GetMapping("/charts")
+    public String charts(){
+        return "charts";
+    }
+
+    @GetMapping("/nav")
+    public String nav(){
+        return "navbar";
+    }
+
     @GetMapping("/register")
     public String register(){
         return "auth/register";
